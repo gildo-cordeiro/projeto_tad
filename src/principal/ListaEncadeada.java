44af03d0a4073d716b8e82a0977af209864c4d00
gildo-cordeiro <gildo.duartec@gmail.com>
2021-04-27 17:41:33 -0300

    last

--- src/principal/ListaEncadeada.java
+++ src/principal/ListaEncadeada.java
@@ -3,6 +3,7 @@ package principal;
 public class ListaEncadeada {
 	private Node first = null, last = null; 
 	
+	//Busca exaustiva
 	public void buscar(int value)	{
 		Node n = first;
 		
@@ -13,11 +14,14 @@ public class ListaEncadeada {
    
 	
 	public void add(Integer value){
+		//declaramos e instanciamos a variavel newNode do tipo Node. seu anterior vai ser 
+		//o fim pois estamos inserindo depois do fim
 		Node newNode = new Node(value);
 		
 		if (first==null)
 			first = last = newNode;
 		else{
+			//seta prox do Node do fim para receber newNode
 			last.setNext(newNode);
 			last = newNode;
 		}
